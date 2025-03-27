4cc5c953fc422cd2862722aef9aa50cb91bc7d57
Abhishek Narayanappa <abhishek.narayanappa@smarterp.com>
2025-03-27 15:13:04 +0530

    Updated .NET version to 8.0

--- obj/Debug/net8.0/AI.AssemblyInfo.cs
+++ obj/Debug/net8.0/AI.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+866c6241d71aa957a1359648c27d8e5fdcea3c20")]
 [assembly: System.Reflection.AssemblyProductAttribute("AI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
